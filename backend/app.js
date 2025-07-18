34e271acf9cc6ef5fc0b32b9cea23d48f4635512
trackmyhomeschool <Chanelle@trackmyhomeschool.com>
2025-07-18 15:16:11 +0500

    solving the cookies issues

--- backend/app.js
+++ backend/app.js
@@ -23,7 +23,7 @@ app.post('/api/payments/webhook', express.raw({ type: 'application/json' }), web
 
 // 2. All normal middleware/routes AFTER
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: ['https://trackmyhomeschool.com', 'http://trackmyhomeschool.com'],
   credentials: true
 }));
 app.use(express.json());
